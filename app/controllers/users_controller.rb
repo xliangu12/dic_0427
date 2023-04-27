9f9bbe5df020a156e549d1e5dde61620d6a38acc
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-27 17:13:32 +0900

    セキュアパスワードの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
 
-  def new
-  end
-
-  def show
-  end
+  before_action :set_user, only: %i[show edit update]
 
   def edit
-    
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを編集しました"
+      redirect_to user_path
+    else
+      flash[:danger] = "プロフィールの編集に失敗しました"
+      render :edit
+    end
   end
 
   def user_params
