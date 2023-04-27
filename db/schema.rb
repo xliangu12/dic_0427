78183fb77478b4d650a8c0bd25af28321b351dbd
she <fangfangshe09@gmail.com>
2023-04-27 17:12:21 +0900

    passwordのカラムを追加,emailのindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_27_064554) do
+ActiveRecord::Schema.define(version: 2023_04_27_080828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2023_04_27_064554) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
